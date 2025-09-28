ed96b7fcb59c548c84ec0fe400a68ae0ac18da3e
ASHUTOSH TIWARI <ashutosh88@amityonline.com>
2025-09-28 14:09:51 +0530

    Fix: Remove invalid ESLint rule for Netlify build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,7 @@
 module.exports = {
   extends: ['next', 'next/core-web-vitals'],
   rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-inline-styles': 'off',
+  '@typescript-eslint/no-explicit-any': 'off',
     // You can add more rules here as needed
   },
 };
