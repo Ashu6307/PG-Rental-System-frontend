38594df87ee5fc4efa49f5779e60b868125e2794
ASHUTOSH TIWARI <ashutosh88@amityonline.com>
2025-09-28 14:06:33 +0530

    Chore: Relax no-explicit-any ESLint rule for build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   extends: ['next', 'next/core-web-vitals'],
   rules: {
-    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-inline-styles': 'off',
     // You can add more rules here as needed
   },
 };
